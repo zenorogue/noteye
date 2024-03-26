42ca9016d624b162538ab5d605967d3a5b95b059
Zeno Rogue <zeno@attnam.com>
2024-03-26 18:13:07 +0100

    increased memory in makeweb.sh

--- hydra/makeweb.sh
+++ hydra/makeweb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-em++ -DEMS hydra.cpp -std=c++11 -o hydra.js -s EXPORTED_FUNCTIONS="['_doit','_webkey','_eachtick']" -lidbfs.js -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']"
+em++ -DEMS hydra.cpp -O2 -fsanitize=address -std=c++11 -o hydra.js -s EXPORTED_FUNCTIONS="['_doit','_webkey','_eachtick']" -lidbfs.js -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']" -sINITIAL_MEMORY=67108864
  
